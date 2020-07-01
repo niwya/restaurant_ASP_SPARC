3585763abd0fff76d24e6638f4dce9a893d03627
Chloe <chloe.benz@outlook.com>
2020-07-01 14:36:58 +0200

    2nd version of SPARC + python program to display part of answer
    Not complete

---
+++ output_reorderer.py
@@ -0,0 +1,26 @@
+## Program to read output from SPARC file and reorganize it to make it more readable ## 
+import subprocess, shlex
+
+command='java -jar sparc.jar restaurant2.sparc -A'
+args=shlex.split(command)
+
+output=subprocess.check_output(args)
+output=str(output)
+output=output[3:-6]
+output_list=output.split(' ')
+
+final_output=[]
+for i in output_list:
+    if i[-1]==',' : i=i[:-1]
+    final_output.append(i)  
+
+# Sorting by keywords #
+keywords=input('Keywords, separated by a comma:\n')
+keywords=keywords.split(',')
+dct={i:[] for i in keywords}
+for i in dct:
+    for j in range(len(final_output)):
+        if i in final_output[j]: dct[i].append(final_output[j])
+print(dct['holds'])
+
+# Displaying it properly #
