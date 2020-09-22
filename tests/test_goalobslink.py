fffe22eea86c4174a9834d49e9f0291e86aaa834
Chloe <chloe.benz@outlook.com>
2020-09-22 10:49:40 -0500

    Folder cleanup

--- tests/test_goalobslink.py
+++
@@ -1,5 +0,0 @@
-# If obs(has_entered(Cu), true, I) is observed then 
-# the goal holds(isattable(Cu, T), J) should be written.
-
-# If obs(bill_wave(Cu), true, I) is observed then 
-# the goal holds(haspaid(Cu), J) should be written.
